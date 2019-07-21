84453af08554d3111b62ce51ed6a95a8f58d5697
Daniel R. Slater <dslater38@gmail.com>
2019-07-21 09:51:45 -0400

    WIP removing more dead 32-bit code

--- src/gdt_flush.s
+++ src/gdt_flush.s
@@ -1,23 +1,5 @@
 [BITS 32]
 
-[GLOBAL gdt_flush]    ; Allows the C code to call gdt_flush().
-
-[GLOBAL idt_flush]    ; Allows the C code to call idt_flush().
-
-gdt_flush:
-	mov eax, [esp+4]  ; Get the pointer to the GDT, passed as a parameter.
-	lgdt [eax]        ; Load the new GDT pointer
-
-	mov ax, 0x10      ; 0x10 is the offset in the GDT to our data segment
-	mov ds, ax        ; Load all data segment selectors
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	jmp 0x08:.flush   ; 0x08 is the offset to our code segment: Far jump!
-.flush:
-	ret
-
 [GLOBAL gdt_flush64]
 gdt_flush64:
 	mov eax, [esp+4] ; Get the pointer to the GDT, passed as a parameter.
@@ -33,18 +15,10 @@ gdt_flush64:
 	ret
 
 
-idt_flush:
-	mov eax, [esp+4]  ; Get the pointer to the IDT, passed as a parameter. 
-	lidt [eax]        ; Load the IDT pointer.
-	ret
-   
-
-
 section .text
 [BITS 64]
 
-
-
+[GLOBAL idt_flush64]
 [EXTERN kmain64]
 
 long_mode_start:
@@ -56,7 +30,6 @@ long_mode_start:
 	jmp .loop
 
 
-[GLOBAL idt_flush64]
 idt_flush64:
 	lidt [rdi]        ; Load the IDT pointer.
 	ret
--- src/kmain.32.cpp
+++ src/kmain.32.cpp
@@ -7,80 +7,17 @@
 #include "timer.h"
 #include "isr.h"
 #include "descriptor_tables.h"
-	
-extern void beep( uint16_t freq );
-extern void vga43(void);
-
-#include "descriptor_tables.h"
-
-
-void helloWorld();
-volatile int foobar = 0;
-
-
 
 extern "C" 
 {
 	
 	extern void gdt_flush64(uint32_t);
-
-	extern uint32_t end;
-	
-	extern uint64_t p4_table[512];
-	extern uint64_t p3_table[512];
-	extern uint64_t p2_table[512];
-	extern uint64_t p1_table[512*4096];
-	
 	extern uint32_t end;
 	extern uint32_t placement_address;
-
-	void page_fault(registers_t regs);
-	void init_gdt_table();
 	static void init_gdt_table64();
 	void init_idt_table();
-	// void initPaging32(uint32_t maxMem);
 	void initPaging64(uint64_t maxMem);
-	void enable_paging_64(void);
-	void enable_paging_64_2(void);
-	
-	
 
-	static void init_monitor()
-	{
-		set_foreground_color32( BLUE );
-		set_background_color32( LTGRAY );
-		monitor_clear32();
-	}
-	
-	static void init_GDT()
-	{
-		monitor_write32("Initialize GDT\n");
-		// init_gdt_table();
-		init_gdt_table64();
-	}
-	
-	static void init_IDT()
-	{
-		monitor_write32("Initialize IDT\n");
-		init_idt_table();
-	}
-		
-	
-	static void handle_page_faults()
-	{
-		monitor_write32("Register Page Fault Handler\n");
-		register_interrupt_handler(14, page_fault);
-	}
-	
-	static void test_page_fault()
-	{
-		monitor_write32("Testing Page Fault\n");
-		uint32_t *ptr = (uint32_t*)0xA0000000;
-		uint32_t do_page_fault = *ptr;
-		monitor_write_dec32(do_page_fault);
-		monitor_write32("After page fault! SHOULDN'T GET HERE \n");
-	}
-	
 	int kmain32(unsigned long magic, multiboot_tag *mboot_ptr)
 	{	
 
