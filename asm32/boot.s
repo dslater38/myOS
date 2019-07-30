55b0343177d8f2c42619ae325f44f3ea6f59b560
Daniel R. Slater <daniel.slater@thermofisher.com>
2019-07-30 12:03:00 -0400

    added commented out code from other branch

--- asm32/boot.s
+++ asm32/boot.s
@@ -127,3 +127,56 @@ p2_table:			; PDE with (PS=1 == 2mb pages, PS == 0 -> points to PTE)
 	dq p
 	%assign p p+ 0x00200000
 	%endrep
+
+
+; Saved from dan/x64_strip branch. 
+; ALternative way to init the GDT.
+; SEGMENT_TYPE_DATA equ 0
+; SEGMENT_ACCESSED equ 1
+; SEGMENT_WRITE_ENABLED equ (1<<1)
+; SEGMENT_EXECUTE_ENABLED equ (1<<1)
+; SEGMENT_EXPAND_DOWN equ (1<<2)
+; SEGMENT_TYPE_CODE equ (1<<3)
+
+; SYSTEM_DESCRIPTOR equ 0
+; NON_SYSTEM_DESCRIPTOR equ (1<<4)
+
+; SEGMENT_PRESENT equ (1<<7)
+
+; RING0	equ 0
+; RING1	equ (1<<5)
+; RING2	equ (2<<5)
+; RING3	equ (3<<5)
+
+
+; IS_64BITS equ (1<<5)
+; OPERAND_SIZE_32 equ (1<<6)
+; GRAN_1K equ (1<<7)
+
+
+; %MACRO GDT 5
+	; dw	(%2 & 0xFFFF)
+	; dw	(%1 & 0xFFFF)
+	; db	(( %1 >> 16 ) & 0xFF)
+	; db	( %3 | %4 | (%5 & NON_SYSTEM_DESCRIPTOR))
+	; db	((( %2 >> 16 ) & 0x0F) | (%5 & 0xE0))
+	; db 	((%1 >> 24) & 0xFF)
+; %ENDMACRO
+
+; gdt_table:
+	; dw	(gdt_table_end - gdt_entries - 1)
+	; dd	0
+	; dd	gdt_entries
+; gdt_entries:
+; .null:
+	; GDT 0, 0, 0, 0, 0
+; .code:
+	; GDT 0, 0xFFFFFFFF, (SEGMENT_PRESENT|SEGMENT_TYPE_CODE|SEGMENT_EXECUTE_ENABLED|SEGMENT_ACCESSED), RING0, (OPERAND_SIZE_32|GRAN_1K|IS_64BITS|NON_SYSTEM_DESCRIPTOR)
+; .data:
+	; GDT 0, 0xFFFFFFFF, (SEGMENT_PRESENT|SEGMENT_TYPE_DATA|SEGMENT_WRITE_ENABLED|SEGMENT_ACCESSED), RING0, (OPERAND_SIZE_32|GRAN_1K|IS_64BITS|NON_SYSTEM_DESCRIPTOR)
+; .user_code:
+	; GDT 0, 0xFFFFFFFF, (SEGMENT_PRESENT|SEGMENT_TYPE_CODE|SEGMENT_EXECUTE_ENABLED), RING3, (OPERAND_SIZE_32|GRAN_1K|IS_64BITS|NON_SYSTEM_DESCRIPTOR)
+; .user_data:
+	; GDT 0, 0xFFFFFFFF, (SEGMENT_PRESENT|SEGMENT_TYPE_DATA|SEGMENT_WRITE_ENABLED), RING3, (OPERAND_SIZE_32|GRAN_1K|IS_64BITS|NON_SYSTEM_DESCRIPTOR)
+; gdt_table_end:
+
