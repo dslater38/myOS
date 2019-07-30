b260342195c17808f0d9c78d69472b865a6443d3
Daniel R. Slater <dslater38@gmail.com>
2019-07-29 20:42:08 -0400

    updates to running

--- run_bochs.sh
+++ run_bochs.sh
@@ -15,12 +15,24 @@
 	#~ bochs -q -dbglog debug.log -f bochsrc.txt
 #~ fi
 
+BOCHS=bochs
+BOCHSRC=bochsrc.txt
 
-if [ "$1" == "-g" ]; then
-/usr/local/bochs269able-avx/bin/bochs -q -f bochsrc-gdb.txt
-else
-
-bochs -q -dbglog debug.log -f bochsrc.txt
-
+if [ "$1" == "-g" -o "$2" == "-g" ]; then
+BOCHS=/usr/local/bochs-gdb/bin/bochs
+BOCHSRC=bochsrc-gdb.txt
 fi
+
+if [ "$1" == "-t" -o "$2" == "-t" ]; then
+BOCHSRC=bochsrc-term.txt
+fi
+
+
+#~ if [ "$1" == "-g" ]; then
+#~ /usr/local/bochs269able-avx/bin/bochs -q -f bochsrc-gdb.txt
+#~ else
+
+${BOCHS} -q -dbglog debug.log -f ${BOCHSRC}
+
+#~ fi
 
